ce1937360c6a663c3ef1e75972a22284ed88118b
kimchungdo <dorian21@naver.com>
2025-05-04 22:51:02 +0900

    set basic structure

---
+++ Tactus/app/src/main/java/com/dorian/tactus/ExerciseFragment.kt
@@ -0,0 +1,31 @@
+package com.dorian.tactus
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dorian.tactus.databinding.FragmentExerciseBinding
+
+class ExerciseFragment : Fragment() {
+
+    private var binding : FragmentExerciseBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentExerciseBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ExerciseFragment()
+    }
+}
